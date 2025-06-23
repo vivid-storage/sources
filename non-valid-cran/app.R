a29237d48b99f5599f3a8d92c332e6ebde03b922
vivid-storage <165187161+vivid-storage@users.noreply.github.com>
2025-06-23 16:08:08 -0700

    Update app.R

--- non-valid-cran/app.R
+++ non-valid-cran/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
   # Application title
-  titlePanel("p3m.dev"),
+  titlePanel("non-valid-cran"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
